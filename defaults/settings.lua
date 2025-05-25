834d6c0c22d23e44927826790f9db1ccdef426a9
blueberrymintz <beelzebufo@icloud.com>
2025-05-25 16:02:55 -0700

    added Cron repo so I can implement clocks. Updated functions and added a way to edit the screen size shape and everything more easily

--- defaults/settings.lua
+++ defaults/settings.lua
@@ -1,13 +1,27 @@
 local debugMode
 ZoomValue = 1
-function InitializeGame(debugMode)
-    if debugMode == 1 then
-    love.window.setMode(270, 480, {resizable=true, vsync=0, minwidth=90, minheight=160})
+function InitializeGame(clearGraphics, debugMode)
+    if clearGraphics == 1 then
     love.graphics.setDefaultFilter("nearest", "nearest")
+    else
     end
-    if debugMode == 2 then
-        love.window.setMode(270, 480, {resizeable=nil, vsync=0, minwidth=90, minheight=160})
-        love.graphics.setDefaultFilter("nearest", "nearest")
+    --wraps to screen size
+        if debugMode == 1 then
+        love.window.setMode(0, 0, {resizable=nil, vsync=0, minwidth=90, minheight=160})
+        end
+    -- for approximate phone screen size, resizeable
+        if debugMode == 2 then
+        love.window.setMode(270, 480, {resizeable=true, vsync=0, minwidth=90, minheight=160})
+        end
+     -- for approximate phone screen size, NOT resizeable
+        if debugMode == 2 then
+        love.window.setMode(270, 480, {resizeable=true, vsync=0, minwidth=90, minheight=160})
+        end
+    -- wraps to screen size, resizeable
+        if debugMode == 3 then 
+        love.wind.setMode(0,0, {resizable=true, vsync= 0})
+        end
 end
 
-end
+
+
--- main.lua
+++ main.lua
@@ -2,12 +2,15 @@ function love.load()
 anim8 = require 'libraries.anim8'
 think = require 'defaults.thinker'
 set = require 'defaults.settings'
-keyInput = require ''
-load_settings(2)
+input = require 'tracker.keyInput'
+cron = require 'libraries/cronLua_Master/cron'
+player = require 'defaults/player'
+ScreenType = {1, 1}
+
 end
 
 function love.update(dt)
-
+local clock = cron.after(1, InitializeGame, ScreenType[1], ScreenType[2])
 end
 
 function love.draw()
