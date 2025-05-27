9d5b3482bd2ef0f8cac09a60bcb0b448dc1ab97b
blueberrymintz <beelzebufo@icloud.com>
2025-05-27 14:39:07 -0700

    still broken

--- main.lua
+++ main.lua
@@ -16,25 +16,28 @@ GTick.track = 1
 GTick.total = 0
 InitializeGame(1,2)
 MousePos = {}
-
+MouseDown = 1
 
 function love.load(dt)
     MousePos.x = 0
     MousePos.y = 0
     Scenery:load()
-
 end
 
 function love.update(dt)
     Scenery:update(dt)
     MousePos.x = love.mouse.getX()
     MousePos.y = love.mouse.getY()
+    function love.mousepressed(x, y, button)
+    if button == 1 then MouseDown = 2 else MouseDown = 1 end
+    end
+    if MouseDown > 1 then 
+        MouseDown = MouseDown - (0.2*dt)
+    end
+
 
 end
 
 function love.draw()
     Scenery:draw()
-end
-function love.mousepressed(x, y, button)
-    
 end
--- scenes/startMenu.lua
+++ scenes/startMenu.lua
@@ -1,6 +1,8 @@
 local game = {}
- 
-
+local down = love.mouse.isDown(1)
+local mousePos = {}
+mousePos.x = 151
+mousePos.y = 551
 function game:load()
     
         startMenu = {}
@@ -14,18 +16,16 @@ end
 
 function game:update(dt)
     startMenu.animations.grow:update((dt*0.5))
-    if MousePos.x > 150 and MousePos.x < 350 and MousePos.y > 550 and MousePos.y < 620 then
+    if mousePos.x > 150 and mousePos.x < 350 and mousePos.y > 550 and mousePos.y < 620 then
         startMenu.gridX = 2
-        if MouseDown == 2 then
-            startMenu.gridX = 4
-        end
 
-    elseif MousePos.x > 150 and MousePos.x < 350 and MousePos.y > 650 and MousePos.y < 720 then
+elseif mousePos.x > 150 and mousePos.x < 350 and mousePos.y > 650 and mousePos.y < 720 then
             startMenu.gridX = 3
+
     else
-        startMenu.gridX = 1
+   
     end
-    startMenu.animations.grow = anim8.newAnimation(startMenu.grid(startMenu.gridX, 1), 0.1)
+
 end
 function game:draw()
     startMenu.animations.grow:draw(startMenu.sprite, 0, 0, nil, 0.5)
@@ -33,5 +33,10 @@ function game:draw()
 end
 
 
-
+        if down then
+            startMenu.gridX = 4
+        end
+                    if down then
+                startMenu.gridX = 5
+            end
 return game
