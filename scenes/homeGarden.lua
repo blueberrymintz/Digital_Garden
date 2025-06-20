ea33c1f43a5a8ebc576f110ab9a64957bbb2c7f2
blueberrymintz <beelzebufo@icloud.com>
2025-06-20 13:50:20 -0700

    starting work on the home garden intro

--- scenes/homeGarden.lua
+++ scenes/homeGarden.lua
@@ -7,19 +7,20 @@ function garden:load()
     windowsill = {}
         -- initializes the start menu sprites and animations
         -- DO NOT DELETE THIS
-windowsill.gridX = 1
+
 windowsill.sprite = love.graphics.newImage('sprites/plantBetter/windowsill.png')
 windowsill.grid = anim8.newGrid(windowsill.sprite:getWidth(), windowsill.sprite:getHeight(), windowsill.sprite:getWidth(), windowsill.sprite:getHeight())
 windowsill.animations = {}
-windowsill.animations.grow = anim8.newAnimation(windowsill.grid(windowsill.gridX, 1), 0.1)
+windowsill.animations.grow = anim8.newAnimation(windowsill.grid(1, 1), 0.1)
 end
 
 
 
 function garden:update(dt)
-        -- necessary. I don't know why but don't delete it, it's the only way we got it to work
-    windowsill.animations.grow = anim8.newAnimation(windowsill.grid(windowsill.gridX, 1), 0.1)
+
 end
+
+
 function garden:draw()
     --background to black
     love.graphics.setBackgroundColor(0,0,0)
