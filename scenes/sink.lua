a75e18056331c93a1a2fb208781b1ece5d35cea6
blueberrymintz <beelzebufo@icloud.com>
2025-06-25 23:49:35 -0700

    added a mug which moves around when the user clicks on the screen

--- scenes/sink.lua
+++ scenes/sink.lua
@@ -16,6 +16,7 @@ function game:load()
     GlobalCurrentScene = "Sink"
     Sink = {}
     SinkKnob = {}
+    Mug = {}
     -- initializes the Sink sprites and animations
     Sink.sprite = love.graphics.newImage('sprites/newSink/sinkSheet.png')
     Sink.grid = anim8.newGrid(Sink.sprite:getWidth()/2, Sink.sprite:getHeight(), Sink.sprite:getWidth(), Sink.sprite:getHeight())
@@ -37,6 +38,16 @@ function game:load()
     SinkKnob.resizeValue = (Screen.resizeValue * OSresizeValue) * 3
     SinkKnob.position = {x = Screen.centerX , y = Screen.centerY }
     SinkKnob.offset = {x = (SinkKnob.sprite:getWidth()/4), y = SinkKnob.sprite:getHeight()}
+
+    Mug.sprite = love.graphics.newImage('sprites/newSink/mug.png')
+    Mug.grid = anim8.newGrid(Mug.sprite:getWidth(), Mug.sprite:getHeight(), Mug.sprite:getWidth(), Mug.sprite:getHeight())
+    Mug.animations = {}
+    Mug.animations.mug = anim8.newAnimation(Mug.grid(1, 1), 0.1)
+    Mug.animations.mug:gotoFrame(1, 1)
+    Mug.resizeValue = (Screen.resizeValue * OSresizeValue) * 3
+    Mug.position = {x = Screen.centerX, y = Screen.centerY}
+    Mug.offset = {x = (Mug.sprite:getWidth()*Mug.resizeValue) / 2, y = (Mug.sprite:getHeight()*Mug.resizeValue) / 2}
+
     local function sinkRunner(string)
     if string == 'flip' then
         if Sink.isOn == true then
@@ -63,14 +74,21 @@ function game:keypressed(key, scancode, isrepeat)
 end
 
 function game:mousepressed(mouseX, mouseY, button)
-    
-
+    if button == 1 then
+        Sink.tick = true
+    end
+    if button then 
+        Mug.position.x = mouseX
+        Mug.position.y = mouseY
+    end
 end 
 
 
 function game:mousereleased(mouseX, mouseY, button)
-
-
+    if button == 1 then
+        Sink.tick = false
+        SinkClock:reset()
+    end
 end
 
 
@@ -89,6 +107,9 @@ function game:draw()
     Sink.animations.background:draw(Sink.sprite, Sink.position.x, Sink.position.y, 0, Sink.resizeValue, Sink.resizeValue, Sink.offset.x, Sink.offset.y)
     -- draw the Sink knob
     SinkKnob.animations.knob:draw(SinkKnob.sprite, SinkKnob.position.x, SinkKnob.position.y, 0, SinkKnob.resizeValue, SinkKnob.resizeValue, SinkKnob.offset.x, SinkKnob.offset.y)
+    -- draw the Mug
+    Mug.animations.mug:draw(Mug.sprite, Mug.position.x, Mug.position.y, 0, Mug.resizeValue, Mug.resizeValue, Mug.offset.x, Mug.offset.y)
 end
+
 
 return game
