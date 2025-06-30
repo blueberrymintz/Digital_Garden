60f37913f5787327690c53e2fdb2ce21cbb55d0e
blueberrymintz <beelzebufo@icloud.com>
2025-06-30 15:34:08 -0700

    added all the sprites needed for the settings menu, started work on arranging them and adding functionality back

--- scenes/settingsMenuRewrite.lua
+++ scenes/settingsMenuRewrite.lua
@@ -25,11 +25,46 @@ function game:load()
     
     sprites.SFX = {}
     sprites.SFX.image = love.graphics.newImage('sprites/settingsMenuRewrite/SFXsprite.png')
-    sprites.SFX.pos = {x = 50 * Screen.resizeValue.w, y = 50 * Screen.resizeValue.h}
+    sprites.SFX.pos = {x = 50 * Screen.resizeValue.w, y = 80 * Screen.resizeValue.h}
     sprites.SFX.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
     sprites.SFX.grid = anim8.newGrid(sprites.SFX.image:getWidth(), sprites.SFX.image:getHeight(), sprites.SFX.image:getWidth(), sprites.SFX.image:getHeight())
+    sprites.SFX.animation = anim8.newAnimation(sprites.SFX.grid(1, 1), 0.1)
+    sprites.SFX.trueWidth = sprites.SFX.image:getWidth() * sprites.SFX.resize.w
+    sprites.SFX.trueHeight = sprites.SFX.image:getHeight() * sprites.SFX.resize.h
+
+    sprites.music = {}
+    sprites.music.image = love.graphics.newImage('sprites/settingsMenuRewrite/musicSprite.png')
+    sprites.music.pos = {x = 50 * Screen.resizeValue.w, y = 180 * Screen.resizeValue.h}
+    sprites.music.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
+    sprites.music.grid = anim8.newGrid(sprites.music.image:getWidth(), sprites.music.image:getHeight(), sprites.music.image:getWidth(), sprites.music.image:getHeight())
+    sprites.music.animation = anim8.newAnimation(sprites.music.grid(1, 1), 0.1)
+    sprites.music.trueWidth = sprites.music.image:getWidth() * sprites.music.resize.w
+    sprites.music.trueHeight = sprites.music.image:getHeight() * sprites.music.resize.h
+    
+    sprites.toggle = {}
+    sprites.toggle.image = love.graphics.newImage('sprites/settingsMenuRewrite/toggleSpriteSheet.png')
+    -- omitting usual sprites pos value to allow for multiple instances of toggle
+    sprites.toggle.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
+    sprites.toggle.grid = anim8.newGrid(sprites.toggle.image:getWidth()/2, sprites.toggle.image:getHeight(), sprites.toggle.image:getWidth(), sprites.toggle.image:getHeight())
+    
+    sprites.toggle.trueWidth = sprites.toggle.image:getWidth() * sprites.toggle.resize.w
+    sprites.toggle.trueHeight = sprites.toggle.image:getHeight() * sprites.toggle.resize.h
+
+    sprites.toggle.animation = {}
+    sprites.toggle.animation.SFX = anim8.newAnimation(sprites.toggle.grid(1, 1), 0.1)
+    sprites.toggle.animation.music = anim8.newAnimation(sprites.toggle.grid(1, 1), 0.1)
+
+    sprites.toggle.pos = {
+        SFX = {x = 250 * Screen.resizeValue.w, y = 80 * Screen.resizeValue.h},
+        music = {x = 250 * Screen.resizeValue.w, y = 180 * Screen.resizeValue.h}
+    }
+
+
+
+
 
     
+
 end
 
 function game:keypressed(key, scancode, isrepeat)
@@ -51,7 +86,10 @@ end
 
 function game:draw()
     sprites.settings.animation:draw(sprites.settings.image, sprites.settings.pos.x, sprites.settings.pos.y, 0, sprites.settings.resize.w, sprites.settings.resize.h)
-    
+    sprites.SFX.animation:draw(sprites.SFX.image, sprites.SFX.pos.x, sprites.SFX.pos.y, 0, sprites.SFX.resize.w, sprites.SFX.resize.h)
+    sprites.music.animation:draw(sprites.music.image, sprites.music.pos.x, sprites.music.pos.y, 0, sprites.music.resize.w, sprites.music.resize.h)
+    sprites.toggle.animation.SFX:draw(sprites.toggle.image, sprites.toggle.pos.SFX.x, sprites.toggle.pos.SFX.y, 0, sprites.toggle.resize.w, sprites.toggle.resize.h)
+    sprites.toggle.animation.music:draw(sprites.toggle.image, sprites.toggle.pos.music.x, sprites.toggle.pos.music.y, 0, sprites.toggle.resize.w, sprites.toggle.resize.h)
 end
 
 return game
