302e6d4f538dbc84a3e97e74c32d0838f933e3ae
blueberrymintz <beelzebufo@icloud.com>
2025-07-01 12:55:41 -0700

    finished more of the icons

--- scenes/icons.lua
+++ scenes/icons.lua
@@ -26,8 +26,8 @@ function game:load()
             y = 50
         }
         sprites.plant.resize = {
-            w = Screen.resizeValue,
-            h = Screen.resizeValue
+            w = Screen.resizeValue * sprites.resize,
+            h = Screen.resizeValue * sprites.resize
         }
         sprites.plant.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
         sprites.plant.trueHeight = sprites.plant.image:getHeight() * sprites.plant.resize.h
@@ -39,16 +39,48 @@ function game:load()
             y = 50
         }
         sprites.lifting.resize = {
-            w = Screen.resizeValue,
-            h = Screen.resizeValue
+            w = Screen.resizeValue * sprites.resize,
+            h = Screen.resizeValue * sprites.resize
         }
         sprites.plant.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
         sprites.plant.trueHeight = sprites.plant.image:getHeight() * sprites.plant.resize.h
-        
-    -- 
+
+    -- park icon
+        sprites.park.grid = anim8.newGrid(sprites.park.image:getWidth(), sprites.lifting.image:getHeight(), sprites.lifting.image:getWidth(), sprites.lifting.image:getHeight())
+        sprites.park.position = {
+            x = 150,
+            y = 50
+        }
+        sprites.park.resize = {
+            w = Screen.resizeValue * sprites.resize,
+            h = Screen.resizeValue * sprites.resize
+        }
+        sprites.park.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
+        sprites.park.trueHeight = sprites.plant.image:getHeight() * sprites.plant.resize.h
+
+    -- store icon
+        sprites.store.grid = anim8.newGrid(sprites.store.image:getWidth(), sprites.store.image:getHeight(), sprites.store.image:getWidth(), sprites.store.image:getHeight())
+        sprites.store.position = {
+            x = 200,
+            y = 50
+        }
+        sprites.store.resize = {
+            w = Screen.resizeValue * sprites.resize,
+            h = Screen.resizeValue * sprites.resize
+        }
+        sprites.store.trueWidth = sprites.store.image:getWidth() * sprites.store.resize.w
+        sprites.store.trueHeight = sprites.store.image:getHeight() * sprites.store.resize.h
+
+    -- watering can icon
+        sprites.waterCan.grid = anim8.newGrid(sprites.waterCan.image:getWidth(). sprites.waterCan.image:getHeight(), sprites.waterCan.image:getWidth(), sprites.waterCan.image:getHeight())
+        sprites.waterCan.position = {
+            x = 250,
+            y = 50
+        }
+        sprites.waterCan.r
     
 
-    
+sprites.resize = 1
 end
 
 function game:keypressed(key, scancode, isrepeat)
