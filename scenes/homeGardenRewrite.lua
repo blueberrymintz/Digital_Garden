e7155c09b522f94cb5d704e95e56433e284bae51
blueberrymintz <beelzebufo@icloud.com>
2025-07-01 20:12:19 -0700

    figured out I don't know how to collapse code on vscode, and started work on the plant tracker values (water, food, everything needed for the care of the plant)

--- scenes/homeGardenRewrite.lua
+++ scenes/homeGardenRewrite.lua
@@ -12,62 +12,67 @@ end
 local sprites = {}
 local buttons = {}
 
+local plants = {}
+plants.tracker = {
+    water = 0
+    
+}
 function game:load()
     print("Warp Successful! Current Scene: Home Garden Rewrite")
     sprites.background = {}
 
     sprites.background.image = love.graphics.newImage('sprites/homeGardenRewrite/plantsBackgroundSprite.png')
-    sprites.background.pos = {x = 0 * Screen.resizeValue.w, y = 0 * Screen.resizeValue.h}
-    sprites.background.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
-    sprites.background.grid = anim8.newGrid(sprites.background.image:getWidth(), sprites.background.image:getHeight(), sprites.background.image:getWidth(), sprites.background.image:getHeight())
-    sprites.background.animation = anim8.newAnimation(sprites.background.grid(1, 1), 0.1)
-    sprites.background.trueWidth = sprites.background.image:getWidth() * sprites.background.resize.w
-    sprites.background.trueHeight = sprites.background.image:getHeight() * sprites.background.resize.h
+        sprites.background.pos = {x = 0 * Screen.resizeValue.w, y = 0 * Screen.resizeValue.h}
+        sprites.background.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
+        sprites.background.grid = anim8.newGrid(sprites.background.image:getWidth(), sprites.background.image:getHeight(), sprites.background.image:getWidth(), sprites.background.image:getHeight())
+        sprites.background.animation = anim8.newAnimation(sprites.background.grid(1, 1), 0.1)
+        sprites.background.trueWidth = sprites.background.image:getWidth() * sprites.background.resize.w
+        sprites.background.trueHeight = sprites.background.image:getHeight() * sprites.background.resize.h
 
     sprites.plant = {}
-    sprites.plant.image = love.graphics.newImage('sprites/homeGardenRewrite/plantSymbolsBW2.png')
-    sprites.plant.pos = {x = 50 * Screen.resizeValue.w, y = 300 * Screen.resizeValue.h}
-    sprites.plant.resize = {w = Screen.resizeValue.w * 0.1, h = Screen.resizeValue.h * 0.1}
-    sprites.plant.grid = anim8.newGrid(sprites.plant.image:getWidth(), sprites.plant.image:getHeight(), sprites.plant.image:getWidth(), sprites.plant.image:getHeight())
-    sprites.plant.animation = anim8.newAnimation(sprites.plant.grid(1, 1), 0.1)
-    sprites.plant.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
-    sprites.plant.trueHeight = sprites.plant.image:getHeight() * sprites.plant.resize.h
+        sprites.plant.image = love.graphics.newImage('sprites/homeGardenRewrite/plantSymbolsBW2.png')
+        sprites.plant.pos = {x = 50 * Screen.resizeValue.w, y = 300 * Screen.resizeValue.h}
+        sprites.plant.resize = {w = Screen.resizeValue.w * 0.1, h = Screen.resizeValue.h * 0.1}
+        sprites.plant.grid = anim8.newGrid(sprites.plant.image:getWidth(), sprites.plant.image:getHeight(), sprites.plant.image:getWidth(), sprites.plant.image:getHeight())
+        sprites.plant.animation = anim8.newAnimation(sprites.plant.grid(1, 1), 0.1)
+        sprites.plant.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
+        sprites.plant.trueHeight = sprites.plant.image:getHeight() * sprites.plant.resize.h
 
     buttons.settings = {}
-    buttons.home = {}
-    buttons.portals = {}
-    buttons.settings.image = love.graphics.newImage('sprites/symbols/gearSymbol.png')
-    buttons.settings.pos = {
+        buttons.home = {}
+        buttons.portals = {}
+        buttons.settings.image = love.graphics.newImage('sprites/symbols/gearSymbol.png')
+        buttons.settings.pos = {
         x = 50,
         y = 600
     }
-    buttons.settings.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
-    buttons.settings.grid = anim8.newGrid(buttons.settings.image:getWidth(), buttons.settings.image:getHeight(), buttons.settings.image:getWidth(), buttons.settings.image:getHeight())
-    buttons.settings.animation = anim8.newAnimation(buttons.settings.grid(1, 1), 0.1)
-    buttons.settings.trueWidth = buttons.settings.image:getWidth() * buttons.settings.resize.w
-    buttons.settings.trueHeight = buttons.settings.image:getHeight() * buttons.settings.resize.h
+        buttons.settings.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
+        buttons.settings.grid = anim8.newGrid(buttons.settings.image:getWidth(), buttons.settings.image:getHeight(), buttons.settings.image:getWidth(), buttons.settings.image:getHeight())
+        buttons.settings.animation = anim8.newAnimation(buttons.settings.grid(1, 1), 0.1)
+        buttons.settings.trueWidth = buttons.settings.image:getWidth() * buttons.settings.resize.w
+        buttons.settings.trueHeight = buttons.settings.image:getHeight() * buttons.settings.resize.h
 
-    buttons.home.image = love.graphics.newImage('sprites/symbols/homeSymbol.png')
-    buttons.home.pos = {
+        buttons.home.image = love.graphics.newImage('sprites/symbols/homeSymbol.png')
+        buttons.home.pos = {
         x = 150,
         y = 700
     }
-    buttons.home.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
-    buttons.home.grid = anim8.newGrid(buttons.home.image:getWidth(), buttons.home.image:getHeight(), buttons.home.image:getWidth(), buttons.home.image:getHeight())
-    buttons.home.animation = anim8.newAnimation(buttons.home.grid(1, 1), 0.1)
-    buttons.home.trueWidth = buttons.home.image:getWidth() * buttons.home.resize.w
-    buttons.home.trueHeight = buttons.home.image:getHeight() * buttons.home.resize.h
+        buttons.home.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
+        buttons.home.grid = anim8.newGrid(buttons.home.image:getWidth(), buttons.home.image:getHeight(), buttons.home.image:getWidth(), buttons.home.image:getHeight())
+        buttons.home.animation = anim8.newAnimation(buttons.home.grid(1, 1), 0.1)
+        buttons.home.trueWidth = buttons.home.image:getWidth() * buttons.home.resize.w
+        buttons.home.trueHeight = buttons.home.image:getHeight() * buttons.home.resize.h
 
-    buttons.portals.image = love.graphics.newImage('sprites/symbols/9circlesSymbol.png')
-    buttons.portals.pos = {
+        buttons.portals.image = love.graphics.newImage('sprites/symbols/9circlesSymbol.png')
+        buttons.portals.pos = {
         x = 250,
         y = 600
     }
-    buttons.portals.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
-    buttons.portals.grid = anim8.newGrid(buttons.portals.image:getWidth(), buttons.portals.image:getHeight(), buttons.portals.image:getWidth(), buttons.portals.image:getHeight())
-    buttons.portals.animation = anim8.newAnimation(buttons.portals.grid(1, 1), 0.1)
-    buttons.portals.trueWidth = buttons.portals.image:getWidth() * buttons.portals.resize.w
-    buttons.portals.trueHeight = buttons.portals.image:getHeight() * buttons.portals.resize.h
+        buttons.portals.resize = {w = Screen.resizeValue.w * 1, h = Screen.resizeValue.h * 1}
+        buttons.portals.grid = anim8.newGrid(buttons.portals.image:getWidth(), buttons.portals.image:getHeight(), buttons.portals.image:getWidth(), buttons.portals.image:getHeight())
+        buttons.portals.animation = anim8.newAnimation(buttons.portals.grid(1, 1), 0.1)
+        buttons.portals.trueWidth = buttons.portals.image:getWidth() * buttons.portals.resize.w
+        buttons.portals.trueHeight = buttons.portals.image:getHeight() * buttons.portals.resize.h
 
 
 
