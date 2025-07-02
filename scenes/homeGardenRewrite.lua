e7f4ad37c7d335450168791ce3defbd4ce6fe1cd
blueberrymintz <beelzebufo@icloud.com>
2025-07-01 18:23:39 -0700

    updated sprites and the home garden; added placeholder apartment room

--- scenes/homeGardenRewrite.lua
+++ scenes/homeGardenRewrite.lua
@@ -11,8 +11,8 @@ local sprites = {}
 function game:load()
     print("Warp Successful! Current Scene: Home Garden Rewrite")
     sprites.background = {}
-    
-    sprites.background.image = love.graphics.newImage('sprites/homeGardenRewrite/homeGardenBackground.png')
+
+    sprites.background.image = love.graphics.newImage('sprites/homeGardenRewrite/plantsBackgroundSprite.png')
     sprites.background.pos = {x = 0 * Screen.resizeValue.w, y = 0 * Screen.resizeValue.h}
     sprites.background.resize = {w = Screen.resizeValue.w, h = Screen.resizeValue.h}
     sprites.background.grid = anim8.newGrid(sprites.background.image:getWidth(), sprites.background.image:getHeight(), sprites.background.image:getWidth(), sprites.background.image:getHeight())
@@ -21,9 +21,9 @@ function game:load()
     sprites.background.trueHeight = sprites.background.image:getHeight() * sprites.background.resize.h
 
     sprites.plant = {}
-    sprites.plant.image = love.graphics.newImage('sprites/homeGardenRewrite/plantSprite.png')
-    sprites.plant.pos = {x = 50 * Screen.resizeValue.w, y = 50 * Screen.resizeValue.h}
-    sprites.plant.resize = {w = Screen.resizeValue.w * 0.2, h = Screen.resizeValue.h * 0.2}
+    sprites.plant.image = love.graphics.newImage('sprites/homeGardenRewrite/plantSymbolsBW2.png')
+    sprites.plant.pos = {x = 50 * Screen.resizeValue.w, y = 300 * Screen.resizeValue.h}
+    sprites.plant.resize = {w = Screen.resizeValue.w * 0.1, h = Screen.resizeValue.h * 0.1}
     sprites.plant.grid = anim8.newGrid(sprites.plant.image:getWidth(), sprites.plant.image:getHeight(), sprites.plant.image:getWidth(), sprites.plant.image:getHeight())
     sprites.plant.animation = anim8.newAnimation(sprites.plant.grid(1, 1), 0.1)
     sprites.plant.trueWidth = sprites.plant.image:getWidth() * sprites.plant.resize.w
@@ -51,7 +51,8 @@ function game:update(dt)
 end
 
 function game:draw()
-    
+    sprites.background.animation:draw(sprites.background.image, sprites.background.pos.x, sprites.background.pos.y, 0, sprites.background.resize.w, sprites.background.resize.h)
+    sprites.plant.animation:draw(sprites.plant.image, sprites.plant.pos.x, sprites.plant.pos.y, 0, sprites.plant.resize.w, sprites.plant.resize.h)
 end
 
 return game
